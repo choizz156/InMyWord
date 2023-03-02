989458ae169a20a2c5e76847e48e2906ba1438c7
cohi <tkfka156@gmail.com>
2023-03-03 00:40:07 +0900

    feat: Cors 설정

---
+++ server/src/main/java/inmyword/server/auth/CorsConfig.java
@@ -0,0 +1,27 @@
+package inmyword.server.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public CorsFilter corsFilter() {
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.addAllowedOriginPattern("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+
+		source.registerCorsConfiguration("/**", corsConfiguration);
+
+		return new CorsFilter(source);
+	}
+
+}

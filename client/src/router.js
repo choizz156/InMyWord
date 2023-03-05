bbb621a79ca3fcb78fe4f868277de8e8c77d7721
cohi <tkfka156@gmail.com>
2023-03-06 00:29:43 +0900

    feat: router 생성

--- client/src/router.js
+++ client/src/router.js
@@ -1,13 +1,24 @@
 import {createWebHistory, createRouter} from "vue-router";
 import Join from "@/components/Join.vue";
+import Login from "@/components/Login.vue";
+import Home from "@/components/Home.vue";
+import LoginHome from "@/components/LoginHome.vue";
 
 const routes = [
     {
         path: "/",
+        component: Home,
+    },
+    {
+        path: "/auth",
+        component: LoginHome,
     },
     {
         path: "/join",
         component: Join
+    }, {
+        path: "/login-form",
+        component: Login
     }
 ];
 

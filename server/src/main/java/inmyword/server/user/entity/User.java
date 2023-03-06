10bbc7687c2885e81dd2bbe557ff8bc248ef3ed5
cohi <tkfka156@gmail.com>
2023-03-07 00:41:02 +0900

    feat: 로그인 유저 조회 메서드 업데이트 메서드 추가

--- server/src/main/java/inmyword/server/user/entity/User.java
+++ server/src/main/java/inmyword/server/user/entity/User.java
@@ -1,7 +1,7 @@
 package inmyword.server.user.entity;
 
 import inmyword.server.audit.BaseEntity;
-import jakarta.persistence.Column;
+import inmyword.server.config.security.PrincipalDetails.UserDetailEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -13,6 +13,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Entity
 @Getter
@@ -20,35 +21,47 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@NotNull
-	private String displayName;
+    @NotNull
+    private String displayName;
 
-	@NotNull
-	private String password;
+    @NotNull
+    private String password;
 
-	@NotNull
-	private  String email;
+    @NotNull
+    private String email;
 
-	@Enumerated(EnumType.STRING)
-	private UserStatus userState = UserStatus.ACTIVE;
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus = UserStatus.ACTIVE;
 
-	@Enumerated(EnumType.STRING)
-	private UserRole role = UserRole.USER;
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole = UserRole.USER;
 
-	@Builder
-	public User(final String displayName, final String password, final String email) {
-		this.displayName = displayName;
-		this.password = password;
-		this.email = email;
-	}
+    @Builder
+    public User(final String displayName, final String password, final String email) {
+        this.displayName = displayName;
+        this.password = password;
+        this.email = email;
+    }
 
-	public void savePwd(final String password) {
-		this.password = password;
-	}
+    public void savePwd(final String password) {
+        this.password = password;
+    }
 
+    public static User loginUser() {
+        UserDetailEntity userEntity =
+            (UserDetailEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userEntity.getUser();
+    }
 
+    public void updateDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void updatePwd(String encodedPwd) {
+        this.password = encodedPwd;
+    }
 }
